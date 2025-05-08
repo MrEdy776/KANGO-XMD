14ddaec5fb1aa9afc8d024415193369a70b03016
MrEdy776 <chinonsokennedy8@gmail.com>
2025-05-08 19:52:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,11 +25,11 @@ global.botname = process.env.BOT_NAME || '𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '2347052838220' 
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'MrEdy' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
